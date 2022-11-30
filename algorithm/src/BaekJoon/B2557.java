0327c34eb701822156b3d921e96609dc0941126c
rkdhs321 <rkdhs321@gmail.com>
2022-11-30 18:59:33 +0900

    백준 알고리즘

--- algorithm/src/BaekJoon/B2557.java
+++ algorithm/src/BaekJoon/B2557.java
@@ -1,7 +1,20 @@
 package BaekJoon;
 
-public class B2557 {
+/*
+문제
+Hello World!를 출력하시오.
 
+입력
+없음
+
+출력
+Hello World!를 출력하시오.
+
+예제 입력 1
+예제 출력 1
+Hello World!
+ */
+public class B2557 {
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
