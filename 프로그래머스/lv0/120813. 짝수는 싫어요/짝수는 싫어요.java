579a933775b3f86394f147616de9da40d3b8b5cd
gaonim <108327813+gaonhub@users.noreply.github.com>
2022-12-21 15:02:48 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = 0;
+        if (n % 2 == 0) {
+            size = n/2;
+        } else {
+            size = n/2 + 1;
+        }
+        int[] answer = new int[size];
+        
+        int index = 0;
+        for (int i=0; i <= n; i++) {
+            if (i % 2 == 1) {
+                answer[index] = i;
+                index++;
+            }
+        }
+        return answer;
+    }
+}
