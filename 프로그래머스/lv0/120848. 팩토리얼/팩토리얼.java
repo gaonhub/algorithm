b867c450548ccc35a987eb392557c1335f12c312
gaonim <108327813+gaonhub@users.noreply.github.com>
2023-01-22 23:05:40 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i=1; i <= 10; i ++) {
+            if (factorial(i) <= n) {
+                answer = i;
+            }
+        }
+        return answer;
+    }
+    
+    int factorial(int n) {
+        if (n<=1) {
+            return 1;
+        } else {
+            return n * factorial(n-1);
+        }
+    }
+}
