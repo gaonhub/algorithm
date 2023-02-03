6a0d5b3a8f2b4e7163b80e20a95240dd901761e6
gaonim <108327813+gaonhub@users.noreply.github.com>
2023-02-03 22:18:46 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.34 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] asc = new int[emergency.length];
+        int[] answer = new int[emergency.length];
+        
+        for(int i=0; i<=emergency.length-1; i++) {
+            asc[i]=emergency[i];
+        }
+        
+        Arrays.sort(asc);
+        
+        for(int i=0; i<=emergency.length-1; i++) {
+            for(int j=0; j<=emergency.length-1; j++) {
+                if(asc[i]==emergency[j]) {
+                    answer[j]=emergency.length-i;
+                }
+            }
+        }
+        return answer;
+    }
+}
